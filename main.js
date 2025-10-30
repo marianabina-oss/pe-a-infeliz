f6c3c1c6e6407b233c1d8237585c397b6b14b14e
marianabina-oss <mariana.bina@escola.pr.gov.br>
2025-10-30 09:25:40 -0300

    h

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const avanca = document.querySelectorAll(".btn-proximo")
 console.log(avanca)
 
-avanca.forEach(button =>{
-    button.addEventListener('click', function(){
-    
+avanca.forEach(button => {
+    button.addEventListener('click', function () {
+        const atual = document.querySelector('.ativo')
+        const proximoPasso = "passo-" + this.getAttribute('data-proximo')
+        atual.classList.remove('ativo')
+        document.getElementById(proximoPasso).classList.add('ativo')
     })
 }
 
